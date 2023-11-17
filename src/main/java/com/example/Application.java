7cdccbc57be84a53126bae0e021ccc8146380830
TrunksCR <117809522+TrunksCR@users.noreply.github.com>
2023-11-17 09:29:23 -0600

    cambios

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,9 +3,20 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal que inicia la aplicación Spring Boot.
+ */
 
 @SpringBootApplication
 public class Application {
+
+	/**
+	 * Método principal que inicia la aplicación Spring Boot.
+	 *
+	 * @param args Los argumentos de línea de comandos proporcionados al iniciar la aplicación.
+	 */
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/com/example/HolaMundo.java
+++ src/main/java/com/example/HolaMundo.java
@@ -3,8 +3,20 @@ package com.example;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controlador que maneja las solicitudes relacionadas con el saludo en la aplicación.
+ */
+
+
 @RestController
 class HolaMundo {
+
+    /**
+     * Maneja las solicitudes a la ruta "/" y devuelve un saludo simple.
+     *
+     * @return Un mensaje de saludo para la ruta "/".
+     */
+
     @RequestMapping("/")
     public String hello() {
         return "project test";
